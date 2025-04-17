b5d300a8ee00eed282d9feb815c4f40e2604a706
EnigmaG <109096125+Brian-Gab@users.noreply.github.com>
2025-04-17 18:33:15 +0800

    Locations added

--- try.py
+++ try.py
@@ -8,6 +8,20 @@ from location_graph import LocationGraph
 hospitals = [
     Location("ACE Malolos Doctors", 14.853554735231283, 120.81142465744408,),
     Location("Bulacan Medical Center/Hospital(BMC)", 14.858302418099049, 120.81745447386649,),
+    Location("Graman Medical Hospital Inc.", 14.84647720458458, 120.83460117593113,),
+    Location("Malolos EENT Hospital", 14.8499986254405, 120.822678718528,),
+    Location("Malolos Maternity Hospital", 14.8501139971557, 120.822242189016,),
+    Location("Malolos San Ildefonso County Hospital", 14.8405069766113, 120.801353264675,),
+    Location("Malolos San Vicente Hospital", 14.8457217693662, 120.817005823084,),
+    Location("Mary Immaculate Maternity and General Hospital", 14.8377610194932, 120.813822697031,),
+    Location("Melissa M. Juico, MD Clinic - Bulacan Provincial Hospital", 14.8569767364998, 120.814434194216,),
+    Location("Ofelia L. Mendoza Maternity & General Hospital (Liang)", 14.8454236766064, 120.813091057165,),
+    Location("Ofelia L. Mendoza Maternity & General Hospital (Mojon)", 14.8674632646949, 120.821274949354,),
+    Location("Romel Cruz Hospital", 14.8110072325401, 120.842409630834,),
+    Location("Sacred Heart Hospital of Malolos", 14.8515663584312, 120.817809870366,),
+    Location("Santos General Hospital", 14.840886581182, 120.81171038819,),
+    Location("St. Michael Clinic and Maternity Hospital", 14.8527404366392, 120.816038676889,),
+    Location("Stma Trinidad Hospital", 14.871006333866, 120.821389699214,),
 ]
 
 schools = [
