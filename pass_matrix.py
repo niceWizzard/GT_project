b368fbad4b4c4103004efa5a32e29066b690127b
RIchard Manansala <richard232manansala@gmail.com>
2025-05-05 22:25:52 +0800

    Converged some close locations

--- pass_matrix.py
+++ pass_matrix.py
@@ -1,4 +1,5 @@
 
+import random
 from location import Location
 import folium
 import pandas as pd
@@ -23,19 +24,19 @@ colors = ["blue", "green", "purple", "orange", "darkred", "lightblue", "darkgree
 locations = [
     Location("St John of God Parish San Rafael", 14.958225524337369, 120.96322267882005),
     Location("Old Municipal Building Baliwag", 14.95481604695801,120.90270723510771),
-    Location("St Augustine Parish Baliwag", 14.955119055786852, 120.90061402905987),
+    # Location("St Augustine Parish Baliwag", 14.955119055786852, 120.90061402905987),
     Location("St James the Apostle Parish Plaridel", 14.885810544266146, 120.85978010412184),
-    Location("Simboryo Chapel of Quingua Plaridel", 14.884845717881191, 120.86089821685681),
+    # Location("Simboryo Chapel of Quingua Plaridel", 14.884845717881191, 120.86089821685681),
     Location("Municipal Trial Court Pulilan", 14.90088287929189, 120.84847772733637, ),
     Location("Parish of St John the Baptist Calumpit", 14.916290963655934, 120.76871365675979),
     Location("Barasoain Church Malolos", 14.846298449087984, 120.81255809227919),
     Location("Immaculate Conception Parish Malolos", 14.842768314252513, 120.81144451558261),
     Location("Museum of Philippine Political History Malolos", 14.844310361974768, 120.81150852779331),
-    Location("Malolos of Aguas Potables Malolos", 14.842713508543106, 120.81231003503166, ),
+    # Location("Malolos of Aguas Potables Malolos", 14.842713508543106, 120.81231003503166, ),
     Location("Hiyas Bulacan Cultural Center and Museum Malolos", 14.855007660211152 , 120.81458057949528),   
-    Location("Bulacan Provincial Capitol Malolos", 14.856600935876505 ,120.81458674303957),
-    Location("Alberta Uitangcoy Santos House Malolos" ,14.839303413513187 ,120.81189263784665),
-    Location("Dr Luis Santos House", 14.841600955916304, 120.81097283034407),
+    # Location("Bulacan Provincial Capitol Malolos", 14.856600935876505 ,120.81458674303957),
+    Location("Alberta Uitangcoy Santos House Malolos" , 14.841424031604319, 120.81056466876372),
+    # Location("Dr Luis Santos House", 14.841600955916304, 120.81097283034407),
     Location("Old PNR  Guiguinto Station Guiguinto", 14.829984932111593 ,120.88392713763665 ),
     Location("Constantino Ancestral HouseBalagtas",14.817061373797769,  120.90780440953162),
     Location("Diocsan Shrine Bulakan",14.795036620289023 ,120.87940245399881),
