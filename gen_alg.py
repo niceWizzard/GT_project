a2dc61a7fd9d2df9681144312fbdbd657b0bcc8c
RIchard Manansala <richard232manansala@gmail.com>
2025-05-07 07:47:28 +0800

    Used the functional genetic algorithm instead of oop

--- gen_alg.py
+++ gen_alg.py
@@ -1,8 +1,9 @@
-from utils.distance import get_distance
 import networkx as nx
 import pandas as pd
-from utils.gen_alg import  GeneticAlgo
+from utils.distance import get_distance
+from utils.genalg import genetic_alg
 import matplotlib.pyplot as plt
+import time
 
 Graph = nx.Graph()
 
@@ -19,19 +20,19 @@ for a in df.itertuples():
 # plt.show()
 
 
-route = GeneticAlgo(Graph, 100, 0.02    , 1000).run()
-
-print(f"Route Distance: {route.length:.2f}m")
+start = time.time()
+tour, distance = genetic_alg(Graph, 100, 0.05, 1000)
+print(f"Finished: {time.time() - start} | Distance: {distance}")
 
 
 new_G = nx.Graph()
 
-for i in range(len(route.cities)):
-    nodeA = route.cities[i]
-    nodeB = route.cities[(i+1)%len(route.cities)]
+for i in range(len(tour)):
+    nodeA = tour[i]
+    nodeB = tour[(i+1)%len(tour)]
     new_G.add_edge(nodeA, nodeB, weight=get_distance(nodeA, nodeB))
 
-pd.DataFrame(route.cities, columns=["Route"]).to_csv("calculations/gen_alg_sol.csv")
+pd.DataFrame(tour, columns=["Route"]).to_csv("calculations/gen_alg_sol.csv")
 
 nx.draw(new_G, with_labels=True, node_size=1000, node_color="skyblue", font_size=10, font_weight="bold")
 plt.show()
