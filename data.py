14f9a15d548d4b773392f745c23f4b38047d139d
RIchard Manansala <richard232manansala@gmail.com>
2025-05-09 21:01:21 +0800

    Updated the coordinates - Removed alberta malolos since it is adjacent to dr luis santos house

--- data.py
+++ data.py
@@ -3,27 +3,27 @@ from utils.location import Location
 colors = ["blue", "green", "purple", "orange", "darkred", "lightblue", "darkgreen", "cadetblue", "pink"]
 
 
-locations_list = [
-	Location("St John of God Parish San Rafael", 14.958225524337369, 120.96322267882005),
-	Location("Old Municipal Building Baliwag", 14.95481604695801,120.90270723510771),
-	Location("St Augustine Parish Baliwag", 14.955119055786852, 120.90061402905987),
-	Location("St James the Apostle Parish Plaridel", 14.885810544266146, 120.85978010412184),
-	Location("Simboryo Chapel of Quingua Plaridel", 14.884845717881191, 120.86089821685681),
-	Location("Municipal Trial Court Pulilan", 14.90088287929189, 120.84847772733637, ),
-	Location("Parish of St John the Baptist Calumpit", 14.916290963655934, 120.76871365675979),
-	Location("Barasoain Church Malolos", 14.846298449087984, 120.81255809227919),
-	Location("Immaculate Conception Parish Malolos", 14.842768314252513, 120.81144451558261),
-	Location("Museum of Philippine Political History Malolos", 14.844310361974768, 120.81150852779331),
-	Location("Malolos of Aguas Potables Malolos", 14.842713508543106, 120.81231003503166, ),
-	Location("Hiyas Bulacan Cultural Center and Museum Malolos", 14.855007660211152 , 120.81458057949528),   
-	Location("Bulacan Provincial Capitol Malolos", 14.856600935876505 ,120.81458674303957),
-	Location("Alberta Uitangcoy Santos House Malolos" , 14.841424031604319, 120.81056466876372),
-	Location("Dr Luis Santos House", 14.841600955916304, 120.81097283034407),
-	Location("Old PNR  Guiguinto Station Guiguinto", 14.829984932111593 ,120.88392713763665 ),
-	Location("Constantino Ancestral HouseBalagtas",14.817061373797769,  120.90780440953162),
-	Location("Diocsan Shrine Bulakan",14.795036620289023 ,120.87940245399881),
-	Location("Old Meycauayan PNR Station Meycauayan" , 14.738682568117946 ,120.96078966260269),
-	Location("St Francis of Aggigi Parish Meycauayan",14.734867083097992 ,120.95717437033578),
+locations_list  = [
+    Location("St John of God Parish San Rafael" , 14.95828110367529	,	120.9636431),
+	Location("Old Municipal Building Baliwag" , 14.954826572586681	,	120.902756),
+	Location("St Augustine Parish Baliwag" , 14.95512348	,	120.9007083),
+	Location("St James the Apostle Parish Plaridel" , 14.88583706	,	120.8600373),
+	Location("Simboryo Chapel of Quingua Plaridel" , 14.88484202	,	120.8608995),
+	Location("Municipal Trial Court Pulilan" , 14.90089071	,	120.8484853),
+	Location("Parish of St John the Baptist Calumpit" , 14.91629545	,	120.7687112),
+	Location("Barasoain Church Malolos" , 14.84630179	,	120.81256),
+	Location("Immaculate Conception Parish Malolos" , 14.84276729	,	120.81145),
+	Location("Museum of Philippine Political History Malolos" , 14.84432717	,	120.8115058),
+	Location("Malolos of Aguas Potables Malolos" , 14.84271351	,	120.81231),
+	Location("Hiyas Bulacan Cultural Center and Museum Malolos" , 14.85501694	,	120.8145829),
+	Location("Bulacan Provincial Capitol Malolos" , 14.85661408	,	120.814589),
+	# Location("Alberta Uitangcoy Santos House Malolos" , 14.84135217	,	120.8105104),
+	Location("Dr Luis Santos House" , 14.84160853	,	120.8109738),
+	Location("Old PNR  Guiguinto Station Guiguinto" , 14.82997485	,	120.8839468),
+	Location("Constantino Ancestral HouseBalagtas" , 14.81698564	,	120.9077197),
+	Location("Diocsan Shrine Bulakan" , 14.795036620289023	,	120.8794025),
+	Location("Old Meycauayan PNR Station Meycauayan" , 14.73868257	,	120.9607897),
+	Location("St Francis of Aggigi Parish Meycauayan" , 14.73486708	,	120.9571744),
 ]
 
 locations_count = len(locations_list)
